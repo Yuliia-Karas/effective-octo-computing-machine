1baa70bbece2afef62caed11592b72e3d8fd882a
Yuliia Karas <117581403+Yuliia-Karas@users.noreply.github.com>
2023-11-10 22:16:30 +0200

    form signup

--- js/signup.js
+++ js/signup.js
@@ -1,9 +1,25 @@
-const form = document.querySelector(".signup-form");
+const registerForm = document.querySelector(".signup-form");
 
-form.addEventListener("submit", (event) => {
+registerForm.addEventListener("submit", handleSubmit);
+
+function handleSubmit(event) {
     event.preventDefault();
-  const {
-    elements: { name, email, password, repeatPassword },
-  } = event.currentTarget;
-  console.log(name.value, email.value, password.value, repeatPassword.value);
-});
+    const form = event.target;
+
+    const name = form.elements.name.value;
+    const email = form.elements.email.value;
+    const password = form.elements.password.value;
+    const repeatPassword = form.elements.repeatPassword.value;
+
+    if (name === "" || email === "" || password === "") {
+        return console.log("Please fill in all the fields!");
+    }
+
+    if (password !== repeatPassword) {
+        return console.log("Your passwords do not match!");
+    }
+    
+    console.log(`Name: ${name}, email:${email}, password:${password}`);
+    form.reset();
+}
+
