d36ee2a186060a53dce2858bfbe8601da9b724f7
Yuliia Karas <117581403+Yuliia-Karas@users.noreply.github.com>
2023-11-12 18:57:17 +0200

    signup request

--- js/signup.js
+++ js/signup.js
@@ -3,23 +3,44 @@ const registerForm = document.querySelector(".signup-form");
 registerForm.addEventListener("submit", handleSubmit);
 
 function handleSubmit(event) {
-    event.preventDefault();
-    const form = event.target;
+  event.preventDefault();
 
-    const name = form.elements.name.value;
-    const email = form.elements.email.value;
-    const password = form.elements.password.value;
-    const repeatPassword = form.elements.repeatPassword.value;
+  const name = document.querySelector('input[name="name"]').value;
+  const email = document.querySelector('input[name="email"]').value;
+  const password = document.querySelector('input[name="password"]').value;
+  const repeatPassword = document.querySelector(
+    'input[name="repeatPassword"]'
+  ).value;
 
-    if (name === "" || email === "" || password === "") {
-        return console.log("Please fill in all the fields!");
-    }
+  if (name === "" || email === "" || password === "") {
+    alert("Please fill in all the fields!");
+    return;
+  }
 
-    if (password !== repeatPassword) {
-        return console.log("Your passwords do not match!");
-    }
-    
-    console.log(`Name: ${name}, email:${email}, password:${password}`);
-    form.reset();
+  if (password !== repeatPassword) {
+    alert("Passwords do not match!");
+    return;
+  }
+
+  const userData = {
+    name: name,
+    email: email,
+    password: password,
+  };
+
+  fetch("https://backend-zb-yk.onrender.com/api/auth/register", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(userData),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+        console.log(data);
+         registerForm.reset();
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
+      
+
 }
-
